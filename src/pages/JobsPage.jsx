ed51a4cae6ce994f69706e482052956e59eb0604
BHANU-BOHRA <120611061+BHANU-BOHRA@users.noreply.github.com>
2025-03-26 09:40:07 +0530

    Add files via upload

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import JobListings from "../components/JobListings";
+
+const JobPage = () => {
+  return (
+    <section className="bg-blue-50 px-4 py-10">
+      <JobListings />
+    </section>
+  );
+};
+
+export default JobPage;
